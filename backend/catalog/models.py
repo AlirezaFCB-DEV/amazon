15d8988bc9035ef11d8797d45caa645166ec20c8
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-02-24 21:04:18 +0330

    feat(catalog.models) : create Category model

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+class Category(models.Model) :
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255 , unique=True)
+    is_active = models.BooleanField(default=True)
+    parent = models.ForeignKey("self" , on_delete=models.PROTECT , null=True , blank=True , related_name="children")
+    
+    class Meta :
+        indexes = [
+            models.Index(fields=["slug"])
+        ]
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category , on_delete=models.PROTECT)
-    attributes = models.JSONField(default=dict , blank=True)
+    attributes = models.JSONField(default=dict , blank=True , db_index=True)
     price = models.DecimalField(max_digits=12 , decimal_places=2)
     
     def __str__(self):
