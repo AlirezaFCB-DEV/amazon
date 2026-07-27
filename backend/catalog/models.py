2479c4af8846226b94c4dd58774813b996d53f14
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-07-27 08:50:36 +0330

    feat : product colors
    -a important property of products

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -15,13 +15,29 @@ class Brand(models.Model) :
     name = models.CharField(max_length=255 , unique=True)
     logo = models.ImageField(upload_to="images/")
     description = models.TextField()
+    
+class ProductColor(models.Model) :
+    color = models.CharField(max_length=10)
+    added_price = models.PositiveIntegerField()
+    
+class ProductSize(models.Model) :
+    size = models.CharField(max_length=10)
+    added_price = models.PositiveIntegerField()
+    
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category , on_delete=models.PROTECT)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255 , unique=True) 
+    category = models.ForeignKey(Category , on_delete=models.PROTECT , related_name="products")
+    color = models.ForeignKey(ProductColor , on_delete=models.CASCADE , related_name="colors")
+    size = models.ForeignKey(ProductSize , on_delete=models.CASCADE , related_name="sizes")
+    
+    
+    brand = models.ForeignKey(Brand , on_delete=models.PROTECT , related_name="products")
     attributes = models.JSONField(default=dict , blank=True , db_index=True)
-    price = models.DecimalField(max_digits=12 , decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    
+    sku = models.CharField(max_length=15 , unique=True)
+    price = models.IntegerField()
     
     def __str__(self):
-        return f"{self.name} - {self.price}"
-    
-    
+        return f"{self.title} - {self.price}"
